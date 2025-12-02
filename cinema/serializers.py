26c88dabc043b85e3464e924bf727babe924d3e7
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-12-02 22:23:03 +0200

    Fix flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -61,7 +61,8 @@ class MovieDetailSerializer(MovieSerializer):
 
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
+        fields = ("id", "title", "description",
+                  "duration", "genres", "actors", "image")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
