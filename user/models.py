6815612df049654afb5ae6fa1bd0db3072963382
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-12-02 21:55:59 +0200

    Solution

--- user/models.py
+++ user/models.py
@@ -1,5 +1,37 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (AbstractUser,
+                                        UserManager as DjangoUserManager)
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(DjangoUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a new `User` with an email, password"""
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    pass
+    """User model"""
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
